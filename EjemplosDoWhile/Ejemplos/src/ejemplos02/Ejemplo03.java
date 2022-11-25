bdf7417377af68ce7d96e44db3abf2d8258901fe
cagonza2@utpl.edu.ec <cagonza2@utpl.edu.ec>
2022-11-25 10:30:53 -0500

    repaso de clase07

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo03.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo03.java
@@ -21,13 +21,16 @@ public class Ejemplo03 {
         double nota;
         boolean bandera = true;
         int salida;
+        double sumaNotas = 0;
         
         do {
             System.out.println("Ingrese calificaciones");
             nota = entrada.nextDouble();
             
+            sumaNotas = sumaNotas + nota;
+            
             cadenaFinal = String.format("%s%.2f\n", cadenaFinal, 
-                    nota);
+                    nota, sumaNotas);
             
             System.out.println("Ingrese (-111) si desea salir del ciclo ");
             salida = entrada.nextInt();
@@ -39,6 +42,7 @@ public class Ejemplo03 {
         } while (bandera); // (bandera==true)
         
         cadenaFinal = String.format("%s\n", cadenaFinal);
-        System.out.printf("Listado de Notas\n%s\n", cadenaFinal);
+        System.out.printf("Listado de Notas\n%s\nSuma de Notas\n%s", 
+                cadenaFinal, sumaNotas);
     }
 }
